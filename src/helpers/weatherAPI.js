68de66ccb6904a2746eb4c62ab647a17a78565a4
PauloFonseca <pfonseca.arq@gmail.com>
2024-01-12 11:29:22 -0300

    Implementando a função searchCities

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,21 @@
 // Remova os comentários a medida que for implementando as funções
+const { VITE_TOKEN } = import.meta.env;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (city) => {
+  const URL_API = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${VITE_TOKEN}&q=${city}`;
+  try {
+    const getCity = await fetch(URL_API).then((response) => response.json())
+      .then((data) => data);
+
+    if (getCity.length === 0) {
+      alert('Nenhuma cidade encontrada');
+    }
+    return getCity;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui!
+  //   seu código aqui!
 };
