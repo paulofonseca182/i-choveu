6302581f2f6cadc52b83f7a774ebc6694b76aaa5
PauloFonseca <pfonseca.arq@gmail.com>
2024-01-12 16:33:22 -0300

    Implementando a funcao getWwatherByCity

--- src/helpers/pageFunctions.js
+++ src/helpers/pageFunctions.js
@@ -1,4 +1,4 @@
-import { searchCities } from './weatherAPI';
+import { searchCities, getWeatherByCity } from './weatherAPI';
 
 /**
  * Cria um elemento HTML com as informações passadas
@@ -110,12 +110,14 @@ export function createCityElement(cityInfo) {
 /**
  * Lida com o evento de submit do formulário de busca
  */
-export function handleSearch(event) {
+export async function handleSearch(event) {
   event.preventDefault();
   clearChildrenById('cities');
 
   const searchInput = document.getElementById('search-input');
   const searchValue = searchInput.value;
-  searchCities(searchValue);
-  // seu código aqui
+  await searchCities(searchValue);
+  const getCities = await searchCities(searchValue);
+  const mapCities = getCities.map((city) => city.url);
+  mapCities.forEach(async (cityUrl) => { await getWeatherByCity(cityUrl); });
 }
--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -16,6 +16,22 @@ export const searchCities = async (city) => {
   }
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-  //   seu código aqui!
+export const getWeatherByCity = async (urlCity) => {
+  const urlCities = `http://api.weatherapi.com/v1/current.json?lang=pt&key=${VITE_TOKEN}&q=${urlCity}`;
+  try {
+    const getCity = await fetch(urlCities).then((response) => response.json())
+      .then((data) => data);
+
+    const response = {
+      temp: getCity.current.temp_c,
+      condition: getCity.current.condition.text,
+      icon: getCity.current.condition.icon,
+      country: getCity.location.country,
+      name: getCity.location.name,
+      url: urlCity,
+    };
+    return response;
+  } catch (error) {
+    return error.message;
+  }
 };
